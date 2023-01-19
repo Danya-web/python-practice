fe20b458872c988b756380fa62adc0c632284a06
Danylo <bratkodan01@gmail.com>
2023-01-19 09:11:22 +0200

    update

---
+++ face detector/main.py
@@ -0,0 +1,16 @@
+import PySimpleGUI as sg
+
+layout = [
+  [sg.Image(key = '-IMAGE-')],
+  [sg.Text('People in picture: 0', key = '')]
+]
+
+sg.Window('Face Detector',layout).read()
+
+while True:
+  event, values = window.read()
+
+  if event == sg.WIN_CLOSED:
+    break
+
+window.close()
